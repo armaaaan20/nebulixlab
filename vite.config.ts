0980ac44b5f2310848fc300c65ddfd289239f11a
armaaaan20 <abdularmaan47@gmail.com>
2025-05-19 13:06:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/nebulixlab",
+  base:"/dark-mode-landing-craft",
   server: {
     host: "::",
     port: 8080,
